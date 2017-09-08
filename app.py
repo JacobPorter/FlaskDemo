8dad116d87d77a33b52f8b0df65efa157bfc4cc5
Jacob Porter <jastuper@gmail.com>
2017-09-08 01:57:08 -0400

    Initial ticker framework

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
 from flask import Flask, render_template, request, redirect
+import pandas as pd
+from bokeh.charts import Histogram
+from bokeh.embed import components
 
-#Jacob Porter
 app = Flask(__name__)
 
+# Load the Iris Data Set
+# iris_df = pd.read_csv("data/iris.data", 
+#     names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"])
+# feature_names = iris_df.columns[0:-1].values.tolist()
+
+# Create the main plot
+# def create_figure(current_feature_name, bins):
+# 	p = Histogram(iris_df, current_feature_name, title=current_feature_name, color='Species', 
+# 	 	bins=bins, legend='top_right', width=600, height=400)
+# 
+# 	# Set the x axis label
+# 	p.xaxis.axis_label = current_feature_name
+# 
+# 	# Set the y axis label
+# 	p.yaxis.axis_label = 'Count'
+# 	return p
+
+# Index page
 @app.route('/')
 def index():
-  return render_template('index.html')
+	# Determine the selected feature
+ 	my_ticker = request.args.get("ticker")
+# 	if current_feature_name == None:
+# 		current_feature_name = "Sepal Length"
+# 
+# 	# Create the plot
+# 	plot = create_figure(current_feature_name, 10)
+# 		
+# 	# Embed plot into HTML via Flask Render
+# 	script, div = components(plot)
+	return render_template("ticker_index.html", ticker = ticker) #, script=script, div=div,
+#		feature_names=feature_names,  current_feature_name=current_feature_name)
 
+# With debug=True, Flask server will auto-reload 
+# when there are code changes
 if __name__ == '__main__':
-  app.run(port=33507)
+	app.run(port=33507, debug=True)
+
