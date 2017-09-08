d53b5e610a4ef1f0297fe495db5a19acff349f3f
Jacob Porter <jastuper@gmail.com>
2017-09-08 04:03:05 -0400

    Stock price close date with quandl

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
-from bokeh.charts import Histogram
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool
 from bokeh.embed import components
+import quandl
+quandl.ApiConfig.api_key = "zhKcDxjgVzwBFH6tyFyh"
 
 app = Flask(__name__)
 
@@ -9,6 +13,20 @@ app = Flask(__name__)
 # iris_df = pd.read_csv("data/iris.data", 
 #     names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"])
 # feature_names = iris_df.columns[0:-1].values.tolist()
+
+def get_data(ticker):
+	#https://www.quandl.com/api/v3/datasets/EOD/AAPL.json?rows=31&order=desc&column_index=4&api_key=YOURAPIKEY
+	data = quandl.get("EOD/" + ticker, rows = 30, column_index = 4, returns = "pandas")
+	return data
+
+def make_plot(ticker, data):
+	p1 = figure(x_axis_type="datetime", title="Stock Closing Prices " + ticker)
+	p1.grid.grid_line_alpha=0.3
+	p1.xaxis.axis_label = 'Date'
+	p1.yaxis.axis_label = 'Price'	
+	p1.line(data.index, data['Close'], color='#A6CEE3', legend=ticker)
+	p1.legend.location = "top_left"
+	return p1
 
 # Create the main plot
 # def create_figure(current_feature_name, bins):
@@ -28,14 +46,14 @@ def index():
 	# Determine the selected feature
  	my_ticker = request.args.get("ticker")
  	if my_ticker == None:
- 		my_ticker = ""
-# 
-# 	# Create the plot
-# 	plot = create_figure(current_feature_name, 10)
-# 		
+ 		my_ticker = "SSNLF"
+ 	data = get_data(my_ticker)
+	plot = make_plot(my_ticker, data)
+	script, div = components(plot)
+	return render_template("ticker_index.html", ticker = my_ticker, script = script, div = div)
 # 	# Embed plot into HTML via Flask Render
-# 	script, div = components(plot)
-	return render_template("ticker_index.html", ticker = my_ticker) #, script=script, div=div,
+ 	
+	#return render_template("ticker_index.html", ticker = my_ticker) #, script=script, div=div,
 #		feature_names=feature_names,  current_feature_name=current_feature_name)
 
 # With debug=True, Flask server will auto-reload 
